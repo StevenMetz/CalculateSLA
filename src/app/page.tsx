48cb48765e2247dfb676aceb8573dc5bb7cf6c35
StevenMetz <stevenm.codes@gmail.com>
2024-11-11 21:13:54 -0600

    Form calculates SLA percentage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,106 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [quater, setQuater] = useState<string>("");
+  const [prevQuater, setPrevQuater] = useState<string>("");
+  const [prevSla, setPrevSla] = useState<string>("");
+  const [totalTickets, setTotalTickets] = useState<number>(0);
+  const [ticketsSLA, setTicketsSLA] = useState<number>(0);
+  const [showCalculations, setShowCalculations] = useState<boolean>(false);
+  const [slaCalc, setSlaCalc] = useState<number>(0);
+  const handleCalculationOfSLA = () => {
+    const calculation = (ticketsSLA / totalTickets) * 100;
+    setShowCalculations(true);
+    setSlaCalc(calculation);
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleCalculationOfSLA();
+    console.log(quater);
+    console.log(totalTickets);
+    console.log(ticketsSLA);
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <main className="container mx-auto">
+      <div className="flex justify-center flex-col">
+        <h1 className="text-center text-7xl mt-20">Calculate SLA</h1>
+        <form className="flex justify-center" onSubmit={handleSubmit}>
+          <div className="flex flex-col w-2/5 justify-center">
+            <div className="p-5 flex justify-center flex-col">
+              <label className="flex justify-center pb-3 text-xl">Previous Quater</label>
+              <input
+                tabIndex={0}
+                aria-label="Previous Quater"
+                type="text"
+                name="previous-quater"
+                placeholder="Q4"
+                className="ml-3 h-10 rounded-md border-gray-600 border-2"
+                value={prevQuater}
+                onChange={(event) => setPrevQuater(String(event.target.value))}
+              />
+            </div>
+            <div className="p-5 flex justify-center flex-col">
+              <label className="flex justify-center pb-3 text-xl">Previous SLA</label>
+              <input
+                aria-label="Previous SLA"
+                name="previous-SLA"
+                placeholder="1.5"
+                className="ml-3 h-10 rounded-md border-gray-600 border-2"
+                value={prevSla}
+                onChange={(event) => setPrevSla(event.target.value)}
+              />
+            </div>
+            <div className="p-5 flex justify-center flex-col">
+              <label className="flex justify-center pb-3 text-xl">Current Quater</label>
+              <input
+                aria-label="Current Quater"
+                name="quater"
+                type="text"
+                placeholder="Q1"
+                className="ml-3 h-10 rounded-md border-gray-600 border-2"
+                value={quater}
+                onChange={(event) => setQuater(String(event.target.value))}
+              />
+            </div>
+            <div className="p-5 flex justify-center flex-col">
+              <label className="flex justify-center pb-3 text-xl">Total Tickets</label>
+              <input
+                aria-label="Total Tickets"
+                name="total-tickets"
+                min="1"
+                className="ml-3 h-10 rounded-md border-gray-600 border-2"
+                placeholder="100"
+                type="number"
+                value={totalTickets}
+                onChange={(event) => setTotalTickets(+event.target.value)}
+              />
+            </div>
+            <div className="p-5 flex justify-center flex-col">
+              <label className="flex justify-center pb-3 text-xl">Tickets outside SLA</label>
+              <input
+                aria-label="input for tickets outside SLA"
+                name="tickets-outside"
+                min="0"
+                type="number"
+                className=" ml-3 h-10 rounded-md border-gray-600 border-2 flex justify-start"
+                value={ticketsSLA}
+                onChange={(event) => setTicketsSLA(+event.target.value)}
+              />
+            </div>
+            <button
+              aria-label="Submit button for SLA form"
+              type="submit"
+              className="border-gray-400 border-2 bg-red-200 flex-initial h-12 w-3/5 mx-auto my-5"
+            >
+              Calculate SLA
+            </button>
+          </div>
+        </form>
+        {showCalculations && (
+          <p className="text-2xl text-red-400 font-extrabold">{`In ${quater}, ${(100 - slaCalc).toFixed(2)}% of all People Services tickets were within the SLA with only ${slaCalc.toFixed(2)}% in violation (${prevQuater} was ${prevSla}%, for comparison).`}</p>
+        )}
+      </div>
+    </main>
   );
 }
