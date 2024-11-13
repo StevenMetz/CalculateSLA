e671571aea833d60e8c60f01b6b6687cb31f086d
StevenMetz <stevenm.codes@gmail.com>
2024-11-12 19:18:44 -0600

    Changed Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Work Tooling",
-  description: "Tools for making work easier",
+  title: "Red Panda Tooling",
+  description: "Tools for making work from your favorite red panda",
 };
 
 export default function RootLayout({
